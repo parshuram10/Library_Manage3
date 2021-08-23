3693642d9c4f8f7b5aaab6119730f3d13305f9cb
parshuram10 <parshuram.patil@prorigosoftware.com>
2021-08-23 19:02:24 +0530

    changes in admin pages , student pages

--- Library Management/db.js
+++ Library Management/db.js
@@ -1,22 +1,49 @@
-var db = openDataBase("itemDB","1.0","itemDB",65535);
-$('.submit-f').click(function(){
+// var db = openDataBase("itemDB","1.0","itemDB",65535);
+// $('.submit-f').click(function(){
 
+//     var name = $('.myName').val();
+//     var email = $('.myEmail').val();
+//     var phone = $('.myPhone').val();
+//     $('#just').text(name+email+phone);
+//     alert("Success");
+//     // db.transaction(function(){
+//     //     var sql = "CREATE TABLE items"+
+//     //     "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"+
+//     //     "name VARCHAR(100) NOT NULL,"+
+//     //     "phone INT(10) NOT NULL)";
+//     //     transaction.executeSql(sql,undefined,function(){
+//     //         alert("Success");
+//     //     },function(){
+//     //         alert("fail");
+//     //     });
+//     // });
+//     // alert("nothing");
+// });
+function addRecord(){
+var mysql = require('mysql')
+
+var con = mysql.createConnection({
+
+    host :"localhost",
+    user:"root",
+    password:"Parshuram@1234",
+    database:"stud"
+})
+
+con.connect(function(err){
+    if(err) throw err;
+    var sql = "INSERT into data values('9','Teja Patil')"
+    con.query(sql,function(err,result){
+    if(err) throw err;
+    console.log("1 entry added")
+    })
+})
+}
+$('.submit-f').click(function(){
     var name = $('.myName').val();
     var email = $('.myEmail').val();
     var phone = $('.myPhone').val();
     $('#just').text(name+email+phone);
-    alert("Success");
-    // db.transaction(function(){
-    //     var sql = "CREATE TABLE items"+
-    //     "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"+
-    //     "name VARCHAR(100) NOT NULL,"+
-    //     "phone INT(10) NOT NULL)";
-    //     transaction.executeSql(sql,undefined,function(){
-    //         alert("Success");
-    //     },function(){
-    //         alert("fail");
-    //     });
-    // });
-    // alert("nothing");
-});
-
+    addRecord();
+    
+})
